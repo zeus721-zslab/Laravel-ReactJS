adea521a7249907f2837a437455be7066be639e8
zeus721 <zeus7211@gmail.com>
2025-04-01 17:27:34 +0900

    general update

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig , loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
-const env = loadEnv(mode, process.cwd(), '');
+export default defineConfig(({ command, mode }) => {
 
-
-export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
 
     return {
@@ -15,9 +13,9 @@ export default defineConfig(({ mode }) => {
             hmr: {
                 host: mode === 'development' ? env.SANCTUM_STATEFUL_DOMAINS_STG : env.SANCTUM_STATEFUL_DOMAINS,  // HMR 엔드포인트
                 protocol: 'ws',  // WebSocket 프로토콜 사용
-                port: mode === 'development' ? env.VITE_APP_PORT : env.VITE_APP_STG_PORT,  // HMR 포트 설정
+                port: mode === 'development' ? parseInt(env.VITE_APP_STG_PORT) : parseInt(env.VITE_APP_PORT),  // HMR 포트 설정
             },
-            port: mode === 'development' ? env.VITE_APP_PORT : env.VITE_APP_STG_PORT,
+            port: mode === 'development' ? parseInt(env.VITE_APP_STG_PORT) : parseInt(env.VITE_APP_PORT),
 
         },
         plugins: [
